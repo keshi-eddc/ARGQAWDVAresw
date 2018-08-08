efca80e411f9d8376f997274677dd60ab93e2737
keshi <keshi.wang@earlydata.com>
2018-08-08 15:37:43 +0800

    update

---
+++ src/main/java/com/edmi/dao/trackico/ICO_trackico_detail_blockTeamRepository.java
@@ -0,0 +1,16 @@
+package com.edmi.dao.trackico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.edmi.entity.trackico.ICO_trackico_detail_blockTeam;
+
+/** 
+* @ClassName: ICO_trackico_detail_blockTeamRepository 
+* @Description: 负责公司人员模型（ICO_trackico_detail_blockTeam） 的数据库操作
+* @author keshi
+* @date 2018年8月6日 上午11:04:13 
+*  
+*/
+public interface ICO_trackico_detail_blockTeamRepository extends JpaRepository<ICO_trackico_detail_blockTeam, Long> {
+
+}
