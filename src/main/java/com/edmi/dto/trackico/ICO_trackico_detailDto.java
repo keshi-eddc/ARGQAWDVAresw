4ed8acc9c0b1f576b6bebbae65d3decfbde646ec
viking <viking.luan@earlydata.com>
2018-08-22 16:48:24 +0800

    add

---
+++ src/main/java/com/edmi/dto/trackico/ICO_trackico_detailDto.java
@@ -0,0 +1,43 @@
+package com.edmi.dto.trackico;
+
+
+
+public class ICO_trackico_detailDto {
+
+	private String block_name;
+	private String block_tag;
+	private String block_description;
+	private String logo_url;
+
+	public String getBlock_name() {
+		return block_name;
+	}
+
+	public void setBlock_name(String block_name) {
+		this.block_name = block_name;
+	}
+
+	public String getBlock_tag() {
+		return block_tag;
+	}
+
+	public void setBlock_tag(String block_tag) {
+		this.block_tag = block_tag;
+	}
+
+	public String getBlock_description() {
+		return block_description;
+	}
+
+	public void setBlock_description(String block_description) {
+		this.block_description = block_description;
+	}
+
+	public String getLogo_url() {
+		return logo_url;
+	}
+
+	public void setLogo_url(String logo_url) {
+		this.logo_url = logo_url;
+	}
+}
---
+++ src/main/java/com/edmi/dto/trackico/ICO_trackico_detail_blockTeamDto.java
@@ -0,0 +1,51 @@
+package com.edmi.dto.trackico;
+
+
+public class ICO_trackico_detail_blockTeamDto {
+
+	private String member_name;
+	private String member_url;
+	private String member_position;
+	private String member_type;
+	private String member_photo_url;
+
+	public String getMember_name() {
+		return member_name;
+	}
+
+	public void setMember_name(String member_name) {
+		this.member_name = member_name;
+	}
+
+	public String getMember_url() {
+		return member_url;
+	}
+
+	public void setMember_url(String member_url) {
+		this.member_url = member_url;
+	}
+
+	public String getMember_position() {
+		return member_position;
+	}
+
+	public void setMember_position(String member_position) {
+		this.member_position = member_position;
+	}
+
+	public String getMember_type() {
+		return member_type;
+	}
+
+	public void setMember_type(String member_type) {
+		this.member_type = member_type;
+	}
+
+	public String getMember_photo_url() {
+		return member_photo_url;
+	}
+
+	public void setMember_photo_url(String member_photo_url) {
+		this.member_photo_url = member_photo_url;
+	}
+}
